fe6904916957e743ed5b5edbd4e4a2bb4c1ad24b
fengjunhui <fengjunhui>
2019-04-04 10:25:39 +0800

    use predis

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticIniteab49b23949b8772622d7fc096cb63b0
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Predis\\' => 7,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Predis\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/predis/predis/src',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticIniteab49b23949b8772622d7fc096cb63b0::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticIniteab49b23949b8772622d7fc096cb63b0::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
